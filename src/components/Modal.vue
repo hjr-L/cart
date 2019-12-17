1dbcc9e9a0a084b124ff0d235b53bcb7946a188e
hjr <1151517639@qq.com>
2019-12-17 10:30:35 +0800

    vuecli实现购物车商品的选中删除增减，以及地址管理 页面

---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+        <div class="modal">
+                <div >
+                        <div class="md-modal modal-msg md-modal-transition " :class="{'md-show':mdShow}">
+                          <div class="md-modal-inner">
+                            <div class="md-top">
+                              <button class="md-close" @click="closeModal">关闭</button>
+                            </div>
+                            <div class="md-content">
+                              <div class="confirm-tips">
+                                <slot name="message"></slot>
+                              </div>
+                              <div class="btn-wrap">
+                                <slot name="btnGroup"></slot>
+                              </div>
+
+                            </div>
+                          </div>
+                        </div>
+                        <div :class="{'md-overlay':mdShow}" @click="closeModal"></div>
+                      </div>
+        </div>
+    </template>
+    
+    <script>
+    export default{
+        name:"nav-modal",
+        data(){
+            return{
+                msg:"modal"
+            }
+    
+        },
+        props:[
+          'mdShow'
+        ],
+        methods:{
+          closeModal(){
+            this.$emit('close');
+          }
+        }
+    }
+    </script>
